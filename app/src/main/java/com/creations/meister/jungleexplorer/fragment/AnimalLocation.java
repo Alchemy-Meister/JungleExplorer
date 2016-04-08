deae258a490f7e0e9b41e1b7b7e7cfd904dfb4a9
Alchemy Meister <jesusesma@gmail.com>
2016-04-08 03:08:36 +0200

    map marker added. Updated to android studio 2.0

--- app/src/main/java/com/creations/meister/jungleexplorer/fragment/AnimalLocation.java
+++ app/src/main/java/com/creations/meister/jungleexplorer/fragment/AnimalLocation.java
@@ -1,45 +1,97 @@
 package com.creations.meister.jungleexplorer.fragment;
 
+import android.location.Address;
+import android.location.Geocoder;
 import android.os.Bundle;
-import android.support.v4.app.Fragment;
-import android.support.v4.app.FragmentManager;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
+import android.text.TextUtils;
+import android.util.Log;
 
 import com.creations.meister.jungleexplorer.R;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.Locale;
 
 /**
  * Created by meister on 4/7/16.
  */
-public class AnimalLocation extends Fragment implements OnMapReadyCallback {
+public class AnimalLocation extends SupportMapFragment implements OnMapReadyCallback,
+        GoogleMap.OnMapClickListener, GoogleMap.OnMarkerDragListener
+{
 
-    SupportMapFragment fragment;
-
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState)
-    {
-        return inflater.inflate(R.layout.location_map, container, false);
-
-    }
+    Marker mMarker;
+    MarkerOptions mMarkerOptions;
+    GoogleMap mMap;
 
     @Override
     public void onActivityCreated(Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
-        FragmentManager fm = this.getActivity().getSupportFragmentManager();
-//        fragment = (SupportMapFragment) fm.findFragmentById(R.id.map);
-//        fragment = SupportMapFragment.newInstance();
-//        fm.beginTransaction().replace(R., fragment).commit();
-        if(fragment != null)
-            fragment.getMapAsync(this);
+
+        this.getMapAsync(this);
     }
 
     @Override
     public void onMapReady(GoogleMap map) {
+        this.mMap = map;
+        map.getUiSettings().setMapToolbarEnabled(false);
+        map.setOnMarkerDragListener(this);
+        map.setOnMapClickListener(this);
+    }
 
+    @Override
+    public void onMapClick(LatLng latLng) {
+
+        if(mMarker == null) {
+            mMarkerOptions = new MarkerOptions()
+                    .position(latLng)
+                    .title("Location")
+                    .snippet(this.getLocationName(latLng))
+                    .draggable(true);
+
+            mMarker = this.mMap.addMarker(mMarkerOptions);
+            mMarker.showInfoWindow();
+
+        }
+    }
+
+    @Override
+    public void onMarkerDragStart(Marker marker) {
+        // Nothing todo here.
+    }
+
+    @Override
+    public void onMarkerDrag(Marker marker) {
+        // Nothing todo here.
+        mMarker.setPosition(marker.getPosition());
+    }
+
+    @Override
+    public void onMarkerDragEnd(Marker marker) {
+        marker.setSnippet(this.getLocationName(marker.getPosition()));
+        marker.showInfoWindow();
+    }
+
+    private String getLocationName(LatLng latLng) {
+        try {
+            Geocoder gcd = new Geocoder(this.getContext(), Locale.getDefault());
+            List<Address> addresses = gcd.getFromLocation(latLng.latitude, latLng.longitude, 1);
+            if (addresses.size() > 0) {
+                if(!TextUtils.isEmpty(addresses.get(0).getLocality())) {
+                    return addresses.get(0).getLocality();
+                } else {
+                    return this.getContext().getResources().getString(R.string.unknown_location);
+                }
+            }
+        } catch (IOException e) {
+            return this.getContext().getResources().getString(R.string.unknown_location);
+        }
+
+        return this.getContext().getResources().getString(R.string.unknown_location);
     }
 }
